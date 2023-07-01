e568bf5c8e852c23fc66a7a02954f51aada823fd
Lucas Bogerd <lucas@youngones.works>
2023-07-01 20:46:44 +0200

    add store styling

---
+++ store.config.ts
@@ -0,0 +1,5 @@
+export const storeConfig = {
+  storeName: "Snelle Planga",
+  storeDescription: "Snelle Planga is een webshop voor de betere zonnebrillen.",
+  svgIcon: "public/logo/snelle-planga.svg",
+}
